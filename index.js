a6e37565dd1cae536259ac26e26493155327c66a
favourachara07 <acharafavour01@gmail.com>
2025-02-02 08:55:53 +0100

    Updates

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const currentTimeElement = document.querySelector('[data-testid="currentTimeUTC"]');
     const currentTime = new Date().toUTCString();
-    currentTimeElement.textContent = `Current Time (UTC): ${currentTime}`;
+    currentTimeElement.textContent =  currentTime
 });
 document.getElementById('dark-mode-toggle').addEventListener('click', function() {
     document.body.classList.toggle('dark-mode');
